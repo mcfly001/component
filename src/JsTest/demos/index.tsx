c5c64ea71c563f0246acb8c3ce17de2566a1edf1
郑泳健 <yongjian.zheng@tongdun.net>
2022-05-26 16:00:05 +0800

    dfa

---
+++ src/JsTest/demos/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { JsTest } from '@/index';
+
+function RankListDemo() {
+  return (
+    <div>
+      <JsTest />
+    </div>
+  );
+}
+
+export default RankListDemo;
---
+++ src/JsTest/index.md
@@ -0,0 +1,18 @@
+---
+title: JsTest 测试js
+nav:
+  title: 组件
+  path: /components
+group:
+  path: /components
+---
+
+# JsTest
+
+用于测试js
+
+## 基础使用
+
+<code src="./demos/index.tsx" />
+
+<API></API>
